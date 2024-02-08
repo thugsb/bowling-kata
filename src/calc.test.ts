b298901945097eeb89041801341df3607f511a02
Stuart Basden <mystery@Stuarts-Air.broadband>
2024-02-08 14:16:59 +0000

    Feature: calculate bowling scores accurately

--- src/calc.test.ts
+++ src/calc.test.ts
@@ -1,9 +1,102 @@
-import { add } from "../src/calc";
-describe("test add function", () => {
-  it("should return 15 for add(10,5)", () => {
-    expect(add(10, 5)).toBe(15);
+import { calcBowlingScore } from "../src/calc";
+describe("test bowling scoring function", () => {
+  it("should handle all possibilities of the last turn", () => {
+    expect(calcBowlingScore([1, "/", 5])).toBe(15);
+    expect(calcBowlingScore([1, "/", "X"])).toBe(20);
+    expect(calcBowlingScore(["X", 1, 2])).toBe(13);
+    expect(calcBowlingScore(["X", 1, "/"])).toBe(20);
+    expect(calcBowlingScore(["X", "X", 2])).toBe(22);
+    expect(calcBowlingScore(["X", "X", "X"])).toBe(30);
   });
-  it("should return 5 for add(2,3)", () => {
-    expect(add(2, 3)).toBe(5);
+  it("should add up all simple numbers", () => {
+    const score = [
+      9,
+      "-",
+      9,
+      "-",
+      9,
+      "-",
+      9,
+      "-",
+      9,
+      "-",
+      9,
+      "-",
+      9,
+      "-",
+      9,
+      "-",
+      9,
+      "-",
+      9,
+      "-",
+    ];
+    expect(calcBowlingScore(score)).toBe(90);
+  });
+  it("should correctly handle spares", () => {
+    const score = [
+      5,
+      "/",
+      5,
+      "/",
+      5,
+      "/",
+      5,
+      "/",
+      5,
+      "/",
+      5,
+      "/",
+      5,
+      "/",
+      5,
+      "/",
+      5,
+      "/",
+      5,
+      "/",
+      5,
+    ];
+    expect(calcBowlingScore(score)).toBe(150);
+  });
+  it("should handle a spare+strike ending", () => {
+    expect(
+      calcBowlingScore([
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        "/",
+        "X",
+      ])
+    ).toBe(18 + 10 + 10);
+  });
+  it("should correctly handle strikes", () => {
+    const score = ["X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X"];
+    expect(calcBowlingScore(score)).toBe(300);
+  });
+  it("should handle strikes that don't finish so well", () => {
+    const score = ["X", "X", "X", "X", "X", "X", "X", "X", "X", "X", 1, 2];
+    expect(calcBowlingScore(score)).toBe(274);
+  });
+  it("should handle a mix of scores", () => {
+    const score = ["X", 4, 3, "X", 4, "/", 7, "/", "X", "X", "X", 2, 3, 4, 5];
+    const calcdTotal = 17 + 7 + 20 + 17 + 20 + 30 + 22 + 15 + 5 + 9;
+    expect(calcBowlingScore(score)).toBe(calcdTotal);
   });
 });
--- src/calc.ts
+++ src/calc.ts
@@ -1,3 +1,66 @@
-export function add(x: number, y: number): number {
-  return x + y;
+export function calcBowlingScore(score: Array<string | number>): number {
+  let total: number = 0;
+  for (let i = 0; i < score.length; i++) {
+    const bowl = score[i];
+    const nextBowl = score[i + 1];
+    const followingBowl = score[i + 2];
+    if (typeof bowl === "number") {
+      // Add simple numbers
+      total += bowl;
+    } else if (bowl === "/") {
+      // Handle spares
+      if (i < score.length - 1) {
+        // A spare as the final bowl is calculated differently
+        const prevBowl = score[i - 1] as number;
+        total += 10 - prevBowl;
+        if (typeof nextBowl === "number") {
+          total += nextBowl;
+        } else {
+          total += 10;
+        }
+      }
+    } else if (bowl === "X") {
+      // Handle strikes
+      if (i < score.length - 2) {
+        // Strikes in the last 2 bowls are calcuated differently
+        total += 10;
+        if (nextBowl === "X" && followingBowl === "X") {
+          total += 20;
+        } else if (nextBowl === "X" && typeof followingBowl === "number") {
+          total += 10 + followingBowl;
+        } else if (typeof nextBowl === "number" && followingBowl === "/") {
+          total += 10;
+        } else if (
+          typeof nextBowl === "number" &&
+          typeof followingBowl === "number"
+        ) {
+          total += nextBowl + followingBowl;
+        }
+      }
+    }
+  }
+
+  // Handle adjustments when the final hit is spare or strike
+  if (score.at(-2) === "/") {
+    // If the player gets a spare on the 2nd-to-last bowl,
+    // they only get the score from it once, not twice,
+    // so we need to adjust for this: (the FOR loop will
+    // have thoughtlessly & incorrectly added the number score)
+    if (typeof score.at(-1) === "number") {
+      total -= score.at(-1) as number;
+    }
+  } else if (score.at(-3) === "X") {
+    // Likewise, if they get a strike on their last turn, adjustments are needed
+    if (typeof score.at(-2) === "number" && typeof score.at(-1) === "number") {
+      total = total - (score.at(-1) as number) - (score.at(-2) as number);
+    }
+    if (typeof score.at(-2) === "number" && score.at(-1) === "/") {
+      total -= score.at(-2) as number;
+    }
+    if (score.at(-2) === "X" && typeof score.at(-1) === "number") {
+      total -= score.at(-1) as number;
+    }
+  }
+  console.log(total);
+  return total;
 }
